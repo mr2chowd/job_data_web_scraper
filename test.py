ccd61c9760619329de99a233b2bbe634a52a949d
Mo Chowdhury <100605327+mr2chowd@users.noreply.github.com>
2025-01-21 10:32:21 -0500

    Update and rename test.java to test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+gender = data.getTheGender() if isLegacyEeInd else data.isWoman() if resolver.resolveObject(Gender, Gender.WOMAN) else resolver.resolveObject(Gender, None)
+aboriginal_code = data.getTheAboriginalCode()
+is_aboriginal = data.isAbrgInd()
+is_visible_minority = data.isDsblInd()
+visible_minorities = data.getTheVisibleMinorities()
+visible_minority_comment = data.getVisminDetail()
+is_disable = data.isDsblInd()
